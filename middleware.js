2a4518a56e998909943ff0feee81a4884dc90f4d
Ramazan Kapar <rmznkapar@gmail.com>
2022-11-19 14:26:32 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log('asjndjkandkajsndjk')
-
   const locales = ['en', 'tr'];
   const defaultLang = 'tr';
   const { headers, nextUrl } = request;
@@ -11,27 +9,34 @@ export function middleware(request) {
 
   const reqLocale = nextUrl.pathname.split("/")[1];
   const noValidLocale = !locales.includes(reqLocale);
-
-
+  
+  console.log(nextUrl.pathname)
   if (shouldCheckLocale && noValidLocale) {
-    const accepts = headers.get("accept-language") || "";
-    // Omit country for now
-    const detected = accepts.split(",")[0].split("-")[0];
-
-    const validLocale = locales.includes(detected) ? detected : defaultLang;
-
-    nextUrl.pathname = `${nextUrl.pathname}`;
-
-    const response = NextResponse.next()
-    response.cookies.set('lang', validLocale)
-    
-    return NextResponse.rewrite(new URL(`/${validLocale}${nextUrl.pathname}`, request.url));
+    console.log('nolocale: '+nextUrl.pathname)
+    return NextResponse.rewrite(new URL(`/${defaultLang}${nextUrl.pathname}`, request.url));
   }
 
-  const response = NextResponse.next()
-  response.cookies.set('lang', reqLocale)
-
   return NextResponse.next();
+
+  // if (shouldCheckLocale && noValidLocale) {
+  //   const accepts = headers.get("accept-language") || "";
+  //   // Omit country for now
+  //   const detected = accepts.split(",")[0].split("-")[0];
+
+  //   const validLocale = locales.includes(detected) ? detected : defaultLang;
+
+  //   nextUrl.pathname = `${nextUrl.pathname}`;
+
+  //   const response = NextResponse.next()
+  //   response.cookies.set('lang', validLocale)
+    
+  //   return NextResponse.rewrite(new URL(`/${validLocale}${nextUrl.pathname}`, request.url));
+  // }
+
+  // const response = NextResponse.next()
+  // response.cookies.set('lang', reqLocale)
+
+  // return NextResponse.next();
 }
 
 export const config = {
